9858192681a593ae773748faeede625b43036743
Jonathan Alyson Salim <jonathan.salim001@binus.ac.id.com>
2025-09-17 17:02:04 +0700

    session2

---
+++ resources/views/fallback.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1
+    style="width: 80%;
+    margin: 100px auto;
+    text-align: center;"
+    >404 NOT FOUND : URL ERROR</h1>
+</body>
+</html>
---
+++ resources/views/users.blade.php
@@ -0,0 +1,2 @@
+<h1>username : {{ $name ? $name}}</h1>
+<h1>age : {{ $age ? $age}}</h1>
